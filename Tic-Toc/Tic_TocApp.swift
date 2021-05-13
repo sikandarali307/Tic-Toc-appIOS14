f683ba643a51b6c67ee285528b6e6b0da29c5f9a
sikandar <sikandar.ali307@gmail.com>
2021-05-13 23:18:47 +0500

    Initial Commit

---
+++ Tic-Toc/Tic_TocApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_TocApp.swift
+//  Tic-Toc
+//
+//  Created by Sikandar Ali on 13/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Tic_TocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
